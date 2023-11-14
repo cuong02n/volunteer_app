f043b091dbbd51406636db8e46b062f6e9a7666b
cuong02n <cuong02n@gmail.com>
2023-11-14 14:48:37 +0700

    re init with spring properties file

---
+++ server/volunteer_backend/src/main/java/vn/edu/hust/volunteer_backend/VolunteerBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hust.volunteer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerBackendApplication.class, args);
+	}
+
+}
