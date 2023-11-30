50115736487e73ee83156b7178566d0177c47b2e
cuong02n <cuong02n@gmail.com>
2023-11-30 10:19:14 +0700

    pre deploy

--- server/src/main/java/vn/edu/hust/volunteer_app/VolunteerApplication.java
+++ server/src/main/java/vn/edu/hust/volunteer_app/VolunteerApplication.java
@@ -2,11 +2,18 @@ package vn.edu.hust.volunteer_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VolunteerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VolunteerApplication.class, args);
+    }
+    @GetMapping("/")
+    public String hello(){
+        return "HELLO !!!!!!!!!!";
     }
 }
